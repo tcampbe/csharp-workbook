8a0652fedecd9ab856e63d4cc6edf04ba7d7ed0c
tcampbe <tcampb@icloud.com>
2019-07-02 19:34:36 -0500

    Rock paper scissors game

---
+++ RockPaperGame/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To Play rock, paper, scissors, ");
+            Console.WriteLine("Enter ");
+            Console.WriteLine("1 for Rock");
+            Console.WriteLine("2 for Paper");
+            Console.WriteLine("3 for Scissors");
+            Console.WriteLine();
+            int hand;
+            hand = Convert.ToInt32(Console.ReadLine());
+            Random generator = new Random();
+            // creates a number 1, 2 or 3
+            int randomNumber = generator.Next(1, 4);
+
+            compareHands(hand, randomNumber);
+            
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("You Picked = " + hand);
+            Console.WriteLine();
+            Console.WriteLine("Computer Picked = " + randomNumber);
+
+
+
+            Console.ReadKey();
+        }
+        public static void compareHands(int hand, int randomNumber)
+        {
+            switch (hand)
+            {
+                case 1:
+                    switch (randomNumber)
+                    {
+                        case 1:
+                            Console.WriteLine("The computer chose Rock, it's a tie!");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("The computer chose paper. Paper covers rock. Computer win!!");
+                            break;
+
+                        case 3:
+                            Console.WriteLine("You are a winner! Your rock smashes the computers scissors!!!");
+                            break;
+
+                        default:
+                            Console.WriteLine("Error. No Winners!");
+                            break;
+                    };
+                    break;
+                case 2:
+                    switch (randomNumber)
+                    {
+                        case 1:
+                            Console.WriteLine("You are a winner! Your paper covers the computers rock!!");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("The computer chose paper, it's a tie!");
+                            break;
+
+                        case 3:
+                            Console.WriteLine("The computer chose scissors. Scissors cut paper. Computer wins!!");
+                            break;
+
+                        default:
+                            Console.WriteLine("Error. No Winners!");
+                            break;
+                    }
+                    break;
+                case 3:
+                    switch (randomNumber)
+                    {
+                        case 1:
+                            Console.WriteLine("The computer chose rock. Rock smashes scissors. Computer wins!!");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("You are a winner! Your scissors cuts the computers paper!!!");
+                            break;
+
+                        case 3:
+                            Console.WriteLine("The computer chose scissors, it's a tie!");
+                            break;
+
+                        default:
+                            Console.WriteLine("Error. No Winners!");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error. No Winners!");
+                    break;
+            }
+        }
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            static string[,] board = new String[9];
+        }
+
+        /*  static string[,] board: this variable represents your 3x3 tic tac toe board
+
+            static String currentPlayer: this variable represents your current player (X or O)
+        */
+
+
+        /*  static void placeMark() : this method should print out the current players 
+                                      turns (X or O), ask the user for their row and 
+                                      column inputs, and update the board.
+
+            static bool isHorizonalWin(): this method should check if the current player
+                                          has won horizontally
+
+            static bool isVerticalWin(): this method should check if the current player 
+                                         has won vertically
+
+            static bool isDiagonalWin(): this method should check if the current player 
+                                         has won diagonally
+
+            static bool hasWon(): this method should check if the current player has won
+
+            static bool isTie(): this methods checks if the game is over and is a tie
+        */
+        //    static void printBoard(): this method should print the board to the console.
+        static void printBoard()
+        {
+            Console.WriteLine("/---|---|---\\");
+            Console.WriteLine("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+            Console.WriteLine("|-----------|");
+            Console.WriteLine("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+            Console.WriteLine("|-----------|");
+            Console.WriteLine("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+            Console.WriteLine("/---|---|---\\");
+        }
+
+    }
+}
